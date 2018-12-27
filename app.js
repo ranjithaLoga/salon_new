c49d808864693ba560ccba04cc4b1f88f8d20e58
ranjithaLoga <ranjithalogaratnam@gmail.com>
2018-12-27 17:38:28 +0530

    minor changes added

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var config = require('./config');
-var index = require('./routes/index');
+var express = require("express");
+var bodyparser = require("body-parser");
+var config = require("./config");
+var index = require("./routes/index");
 
 var app = express();
 app.use(bodyparser.json());
 
-app.use('/', index);
+app.use("/", index);
 
 //catch 404 and forward to error handler
 
-app.use(function (req, res, next) {
-    var err = new Error('Not found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error("Not found");
+  err.status = 404;
+  next(err);
 });
 
 //error handler
-app.use(function (err, req, res, next) {
-    //set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    //render error page
+  //render error page
 
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: err
-        });
-
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 if (!module.parent) {
-    app.listen(config.port, function () {
-        console.log(`app listening at http://localhost:${config.port}`);
-    });
+  app.listen(config.port, function() {
+    console.log(`app listening at http://localhost:${config.port}`);
+  });
 }
 
 module.exports = app;
